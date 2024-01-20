1e8c0e6e591c4750257f4174c725a1324c7a22cf
hari saputra <hari.saputra.dev@gmail.com>
2024-01-20 17:08:56 +0700

    make module, repo, service (user n product), setup db sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
