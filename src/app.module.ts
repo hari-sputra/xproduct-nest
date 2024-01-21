df1ab71fef2ba3218b691baa55c8b77def91bc62
hari saputra <hari.saputra.dev@gmail.com>
2024-01-21 15:02:57 +0700

    add entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Product } from './products/product.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User, Product],
       synchronize: true,
     }),
     UsersModule,
